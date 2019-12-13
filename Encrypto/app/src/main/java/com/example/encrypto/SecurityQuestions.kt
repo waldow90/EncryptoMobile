c647fd27cafb293df0599829d0fea032d8cbc068
Daantje1041 <57103897+Daantje1041@users.noreply.github.com>
2019-12-13 13:43:48 +0100

    EDIT SecurityQuestions, cant send empty answers anymore, max 3 times wrong answers

--- Encrypto/app/src/main/java/com/example/encrypto/SecurityQuestions.kt
+++ Encrypto/app/src/main/java/com/example/encrypto/SecurityQuestions.kt
@@ -22,22 +22,39 @@ class SecurityQuestions : AppCompatActivity() {
         sqQ2.text = ManageDB().getSecurityQuestion(this, 3)
         sqQ3.text = ManageDB().getSecurityQuestion(this, 4)
 
-        btnMe.setOnClickListener{
-            if
-            (
-                ManageDB().checkSecurityQuestion(this, checkA1.text.toString(), 2) &&
-                ManageDB().checkSecurityQuestion(this, checkA2.text.toString(), 3) &&
-                ManageDB().checkSecurityQuestion(this, checkA3.text.toString(), 4)
-            )
-            {
-                startActivity(Intent(this, ResetPIN::class.java))
-                finish()
-            }
-            else
-            {
-                Toast.makeText(this, "This is not you!", Toast.LENGTH_LONG).show()
+        var wrong = 0
+
+        btnMe.setOnClickListener {
+            when {
+                checkA1.text.length < 2 || checkA1.text.length > 31 -> {
+                    checkA1.setText("")
+                    checkA1.setHintTextColor(Color.RED)
+                    checkA1.hint = getString(R.string.answer_cannot_be_empty)
+                }
+                checkA2.text.length < 2 || checkA2.text.length > 31 -> {
+                    checkA2.setText("")
+                    checkA2.setHintTextColor(Color.RED)
+                    checkA2.hint = getString(R.string.answer_cannot_be_empty)
+                }
+                checkA3.text.length < 2 || checkA3.text.length > 31 -> {
+                    checkA3.setText("")
+                    checkA3.setHintTextColor(Color.RED)
+                    checkA3.hint = getString(R.string.answer_cannot_be_empty)
+                }
+                else -> {
+                    if (
+                        ManageDB().checkSecurityQuestion(this, checkA1.text.toString(), 2) &&
+                        ManageDB().checkSecurityQuestion(this, checkA2.text.toString(), 3) &&
+                        ManageDB().checkSecurityQuestion(this, checkA3.text.toString(), 4)
+                    ) {
+                        startActivity(Intent(this, ResetPIN::class.java))
+                        finish()
+                    } else {
+                        Toast.makeText(this, "This is not you!", Toast.LENGTH_LONG).show()
+                        wrong++
+                    }
+                }
             }
         }
     }
-
 }
